a20133b89421de3d22b587350cfb4a50a85521db
neimarbl <neimarbisewski@gmail.com>
2026-06-24 10:13:53 -0300

    feat(frontend): implement tailwind.config and postcss configs with custom animation keyframes

---
+++ frontend/postcss.config.js
@@ -0,0 +1,7 @@
+// frontend/postcss.config.js
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}, // Injeta prefixos de navegadores antigos automaticamente (ótimo para PWAs em celulares antigos)
+  },
+}
